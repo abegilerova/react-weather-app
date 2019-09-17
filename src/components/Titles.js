6db0e28405487f9a207e9f53271a53f6d1af4530
abegilerova <abegilerova@gmail.com>
2019-09-16 21:21:41 -0400

    regards

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -7,7 +7,7 @@ class Titles extends React.Component {
     render() {
         return (
             <div>
-                <h1>"For Ais, Diana, Rita </h1>
+                <h3 className="title-container__subtitle">For Ais, Diana, Rita </h3>
                 <h2 className="title-container__title">Weather Finder</h2>
                 <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
 
