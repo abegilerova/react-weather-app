aa1e01d2d2fede9b313724c5729284f4c9ccd14f
abegilerova <abegilerova@gmail.com>
2019-09-16 21:18:42 -0400

    dedication: git commit -m dedication
    exit
    git commit -m dedication:

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -7,7 +7,8 @@ class Titles extends React.Component {
     render() {
         return (
             <div>
-                <h1 className="title-container__title">Weather Finder</h1>
+                <h1>"For Ais, Diana, Rita </h1>
+                <h2 className="title-container__title">Weather Finder</h2>
                 <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
 
             </div>
