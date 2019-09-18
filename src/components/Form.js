a76dd3ff9471c24ab404c6272bc1367f559e83cc
abegilerova <abegilerova@gmail.com>
2019-09-18 19:36:11 -0400

    adding class to input directly

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ class Form extends React.Component {
     render() {
         return (
             <form clssName="formWeather" onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="city" />
-                <input type="text" name="country" placeholder="country" />
+                <input className="inputForm" type="text" name="city" placeholder="city" />
+                <input className="inputForm" type="text" name="country" placeholder="country" />
                 <button>Get Weather</button>
             </form>
         );
